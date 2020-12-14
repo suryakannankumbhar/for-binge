40f463800b810e11c02f5ee0c14f3dad7d0bd181
Surya Kannan Kumbhar <suryakannankumbhar@gmail.com>
2020-12-14 12:15:58 +0530

    update - database updated

--- src/database.js
+++ src/database.js
@@ -49,6 +49,40 @@ export const database = {
         "https://www.wallpapertip.com/wmimgs/1-10435_attack-on-titan-season-3-wallpaper-hd.png"
     },
     {
+      name: "My Hero Academia",
+      releaseDate: "2016",
+      ratingIMDb: "8.5/10",
+      director: "Kohei Horikoshi",
+      poster:
+        "https://files.wallpaperpass.com/2019/10/bnha%20wallpaper%20186%20-%201920x1080.jpg"
+    },
+    {
+      name: "One Punch Man",
+      releaseDate: "2015",
+      ratingIMDb: "8.8/10",
+      director: "Yusuke Murata",
+      poster:
+        "https://www.wallpapertip.com/wmimgs/158-1581414_one-punch-man-s-class-heroes-characters-4k.jpg"
+    },
+    {
+      name: "Avatar: The Last Airbender",
+      releaseDate: "2005",
+      ratingIMDb: "9.2/10",
+      director: "Michael Dante DiMartino and Bryan Konietzko",
+      poster:
+        "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/5298bac0-b8bf-4c80-af67-725c1272dbb0/de1jch0-0627d112-5c34-44ec-bc37-d69296047420.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvNTI5OGJhYzAtYjhiZi00YzgwLWFmNjctNzI1YzEyNzJkYmIwXC9kZTFqY2gwLTA2MjdkMTEyLTVjMzQtNDRlYy1iYzM3LWQ2OTI5NjA0NzQyMC5qcGcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.pEF4PgsM9TPST-BEybkOaRgDwg7h8CN_ejnt5zyNPTU"
+    },
+    {
+      name: "Inazuma Eleven",
+      releaseDate: "2008",
+      ratingIMDb: "8/10",
+      director: "Tenya Yabuno",
+      poster: "https://wallpaperaccess.com/full/1623339.jpg"
+    },
+    // ----------------------------------------------------------------------
+    // -----------------------------MARVEL-----------------------------------
+    // ----------------------------------------------------------------------
+    {
       name: "Iron Man",
       releaseDate: "2008",
       ratingIMDb: "7.9/10",
@@ -226,9 +260,9 @@ export const database = {
         "https://cdn1-www.superherohype.com/assets/uploads/gallery/spider-man-far-from-home/spider-man-far-from-home-poster.jpg"
     },
 
-    // DC---------------------------------------------------------------------------
-    // ----------------------------------------------------------------------------------
-    // ------------------------------------------------------------------------------
+    // -----------------------------------------------------------------------------
+    // -------------------------------------DC--------------------------------------
+    // -----------------------------------------------------------------------------
     {
       name: "Batman Begins",
       releaseDate: "2005",
@@ -251,6 +285,72 @@ export const database = {
       poster:
         "https://i.pinimg.com/originals/ac/7b/13/ac7b13431c8acd1031333351669f09d0.jpg"
     },
+    {
+      name: "Man Of Steel",
+      releaseDate: "2013",
+      ratingIMDb: "7/10",
+      director: "Zack Snyder",
+      poster: "https://wallpapercave.com/wp/M0BdpjB.jpg"
+    },
+    {
+      name: "Batman v Superman: Dawn of Justice",
+      releaseDate: "2016",
+      ratingIMDb: "6.4/10",
+      director: "Zack Snyder",
+      poster:
+        "https://static1.cbrimages.com/wordpress/wp-content/uploads/2018/05/Batman-v-Superman.jpg"
+    },
+    {
+      name: "Suicide Squad",
+      releaseDate: "2016",
+      ratingIMDb: "6/10",
+      director: "David Ayer",
+      poster:
+        "https://townsquare.media/site/442/files/2016/06/suicidesquadposters-featured.jpg?w=980&q=75"
+    },
+    {
+      name: "Wonder Woman",
+      releaseDate: "2017",
+      ratingIMDb: "7.4/10",
+      director: "Patty Jenkins",
+      poster:
+        "https://www.chimerascomics.com/wp-content/uploads/2017/06/1491990205555-1.jpg"
+    },
+    {
+      name: "Justice League",
+      releaseDate: "2017",
+      ratingIMDb: "6.3/10",
+      director: "Joss Whedon and Zack Snyder",
+      poster:
+        "https://s3-us-east-2.amazonaws.com/redefined/wp-content/uploads/2020/06/18151955/justiceleague_wb.jpg"
+    },
+    {
+      name: "Aquaman",
+      releaseDate: "2018",
+      ratingIMDb: "6.9/10",
+      director: "James Wan",
+      poster:
+        "https://images.indianexpress.com/2018/07/aquaman-jason-momoa-1200.jpg"
+    },
+    {
+      name: "Shazam!",
+      releaseDate: "2019",
+      ratingIMDb: "7.1/10",
+      director: "David F. Sandberg",
+      poster:
+        "https://see.news/wp-content/uploads/2020/05/7105962-0-image-a-101_1544126019034.jpg"
+    },
+    {
+      name: "Harley Quinn: Birds of Prey",
+      releaseDate: "2020",
+      ratingIMDb: "6.1/10",
+      director: "Cathy Yan",
+      poster:
+        "https://cdn.flickeringmyth.com/wp-content/uploads/2020/02/birds-of-prey-poster.jpg"
+    },
+    // --------------------------------------------------------------------------
+    // ---------------------------------sci-fi-----------------------------------
+    // --------------------------------------------------------------------------
     {
       name: "Inception",
       releaseDate: "2010",
@@ -279,6 +379,61 @@ export const database = {
       director: "Christopher Nolan",
       poster:
         "https://desktopwalls.net/wp-content/uploads/2015/03/Interstellar%20Movie%20Satellite%20Desktop%20Wallpaper.jpg"
+    },
+    {
+      name: "Gravity",
+      releaseDate: "2013",
+      ratingIMDb: "7.7/10",
+      director: "Alfonso Cuarón",
+      poster:
+        "https://www.joblo.com/assets/images/oldsite/posters/images/full/gravity-quad-poster1.jpg"
+    },
+    {
+      name: "The Martian",
+      releaseDate: "2015",
+      ratingIMDb: "8/10",
+      director: "Ridley Scott",
+      poster:
+        "https://thumbor.forbes.com/thumbor/fit-in/1200x0/filters%3Aformat%28jpg%29/https%3A%2F%2Fblogs-images.forbes.com%2Fscottmendelson%2Ffiles%2F2015%2F09%2FTHE-MARTIAN-movie-poster2.jpg"
+    },
+    {
+      name: "The Matrix",
+      releaseDate: "1999",
+      ratingIMDb: "8.7/10",
+      director: "Lana Wachowski and Lilly Wachowski",
+      poster:
+        "https://i.pinimg.com/originals/24/b2/91/24b291caa2aab90e6447423b63430466.jpg"
+    },
+    {
+      name: "Tenet",
+      releaseDate: "2020",
+      ratingIMDb: "7.6/10",
+      director: "Christopher Nolan",
+      poster:
+        "https://www.vitalthrills.com/wp-content/uploads/2020/07/tenetheader1.jpg"
+    },
+    {
+      name: "Oblivion",
+      releaseDate: "2013",
+      ratingIMDb: "7/10",
+      director: "Joseph Kosinski",
+      poster:
+        "https://images.squarespace-cdn.com/content/v1/507b2f30e4b066e116488db6/1366581664395-1EOF0V8KL1COY6OENC67/ke17ZwdGBToddI8pDm48kFXWtxZx0wf_p1RfWpktiCl7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QPOohDIaIeljMHgDF5CVlOqpeNLcJ80NK65_fV7S1UaFDO7toaSPuTnNia-BQWpSVwvBv5jrFXkgB0ixCLun2eDZ_p8TdFHKRYejNqpIBCA/Oblivion-Poster.jpg"
+    },
+    {
+      name: "After Earth",
+      releaseDate: "2013",
+      ratingIMDb: "4.8/10",
+      director: "M. Night Shyamalan",
+      poster: "https://vistapointe.net/images/after-earth-3.jpg"
+    },
+    {
+      name: "Jurassic Park",
+      releaseDate: "1993",
+      ratingIMDb: "8.1/10",
+      director: "Steven Spielberg",
+      poster:
+        "https://i.pinimg.com/originals/44/c6/d4/44c6d42e92e7c399ad3aa35260da1c5b.jpg"
     }
   ],
   marvel: [
@@ -482,6 +637,69 @@ export const database = {
       director: "Christopher Nolan",
       poster:
         "https://i.pinimg.com/originals/ac/7b/13/ac7b13431c8acd1031333351669f09d0.jpg"
+    },
+    {
+      name: "Man Of Steel",
+      releaseDate: "2013",
+      ratingIMDb: "7/10",
+      director: "Zack Snyder",
+      poster: "https://wallpapercave.com/wp/M0BdpjB.jpg"
+    },
+    {
+      name: "Batman v Superman: Dawn of Justice",
+      releaseDate: "2016",
+      ratingIMDb: "6.4/10",
+      director: "Zack Snyder",
+      poster:
+        "https://static1.cbrimages.com/wordpress/wp-content/uploads/2018/05/Batman-v-Superman.jpg"
+    },
+    {
+      name: "Suicide Squad",
+      releaseDate: "2016",
+      ratingIMDb: "6/10",
+      director: "David Ayer",
+      poster:
+        "https://townsquare.media/site/442/files/2016/06/suicidesquadposters-featured.jpg?w=980&q=75"
+    },
+    {
+      name: "Wonder Woman",
+      releaseDate: "2017",
+      ratingIMDb: "7.4/10",
+      director: "Patty Jenkins",
+      poster:
+        "https://www.chimerascomics.com/wp-content/uploads/2017/06/1491990205555-1.jpg"
+    },
+    {
+      name: "Justice League",
+      releaseDate: "2017",
+      ratingIMDb: "6.3/10",
+      director: "Joss Whedon and Zack Snyder",
+      poster:
+        "https://s3-us-east-2.amazonaws.com/redefined/wp-content/uploads/2020/06/18151955/justiceleague_wb.jpg"
+    },
+    {
+      name: "Aquaman",
+      releaseDate: "2018",
+      ratingIMDb: "6.9/10",
+      director: "James Wan",
+      poster:
+        "https://images.indianexpress.com/2018/07/aquaman-jason-momoa-1200.jpg"
+    },
+    {
+      name: "Shazam!",
+      releaseDate: "2019",
+      ratingIMDb: "7.1/10",
+      director: "David F. Sandberg",
+      poster:
+        "https://see.news/wp-content/uploads/2020/05/7105962-0-image-a-101_1544126019034.jpg"
+    },
+    {
+      name: "Harley Quinn: Birds of Prey",
+      releaseDate: "2020",
+      ratingIMDb: "6.1/10",
+      director: "Cathy Yan",
+      poster:
+        "https://cdn.flickeringmyth.com/wp-content/uploads/2020/02/birds-of-prey-poster.jpg"
     }
   ],
   "sci-fi": [
@@ -513,6 +731,61 @@ export const database = {
       director: "Christopher Nolan",
       poster:
         "https://desktopwalls.net/wp-content/uploads/2015/03/Interstellar%20Movie%20Satellite%20Desktop%20Wallpaper.jpg"
+    },
+    {
+      name: "Gravity",
+      releaseDate: "2013",
+      ratingIMDb: "7.7/10",
+      director: "Alfonso Cuarón",
+      poster:
+        "https://www.joblo.com/assets/images/oldsite/posters/images/full/gravity-quad-poster1.jpg"
+    },
+    {
+      name: "The Martian",
+      releaseDate: "2015",
+      ratingIMDb: "8/10",
+      director: "Ridley Scott",
+      poster:
+        "https://thumbor.forbes.com/thumbor/fit-in/1200x0/filters%3Aformat%28jpg%29/https%3A%2F%2Fblogs-images.forbes.com%2Fscottmendelson%2Ffiles%2F2015%2F09%2FTHE-MARTIAN-movie-poster2.jpg"
+    },
+    {
+      name: "The Matrix",
+      releaseDate: "1999",
+      ratingIMDb: "8.7/10",
+      director: "Lana Wachowski and Lilly Wachowski",
+      poster:
+        "https://i.pinimg.com/originals/24/b2/91/24b291caa2aab90e6447423b63430466.jpg"
+    },
+    {
+      name: "Tenet",
+      releaseDate: "2020",
+      ratingIMDb: "7.6/10",
+      director: "Christopher Nolan",
+      poster:
+        "https://www.vitalthrills.com/wp-content/uploads/2020/07/tenetheader1.jpg"
+    },
+    {
+      name: "Oblivion",
+      releaseDate: "2013",
+      ratingIMDb: "7/10",
+      director: "Joseph Kosinski",
+      poster:
+        "https://images.squarespace-cdn.com/content/v1/507b2f30e4b066e116488db6/1366581664395-1EOF0V8KL1COY6OENC67/ke17ZwdGBToddI8pDm48kFXWtxZx0wf_p1RfWpktiCl7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QPOohDIaIeljMHgDF5CVlOqpeNLcJ80NK65_fV7S1UaFDO7toaSPuTnNia-BQWpSVwvBv5jrFXkgB0ixCLun2eDZ_p8TdFHKRYejNqpIBCA/Oblivion-Poster.jpg"
+    },
+    {
+      name: "After Earth",
+      releaseDate: "2013",
+      ratingIMDb: "4.8/10",
+      director: "M. Night Shyamalan",
+      poster: "https://vistapointe.net/images/after-earth-3.jpg"
+    },
+    {
+      name: "Jurassic Park",
+      releaseDate: "1993",
+      ratingIMDb: "8.1/10",
+      director: "Steven Spielberg",
+      poster:
+        "https://i.pinimg.com/originals/44/c6/d4/44c6d42e92e7c399ad3aa35260da1c5b.jpg"
     }
   ],
   anime: [
@@ -547,6 +820,53 @@ export const database = {
       director: "Ichirō Ōkouchi",
       poster:
         "https://images-na.ssl-images-amazon.com/images/I/716S4vU4vLL._AC_SL1039_.jpg"
+    },
+    {
+      name: "Tokyo Ghoul",
+      releaseDate: "2014",
+      ratingIMDb: "7.9/10",
+      director: "Sui Ishida",
+      poster:
+        "https://toonamifaithful.com/wp-content/uploads/2017/03/tokyo-ghoul-pictures_095921755_290.jpg"
+    },
+    {
+      name: "Attack On Titan",
+      releaseDate: "2013",
+      ratingIMDb: "8.8/10",
+      director: "Hajime Isayama",
+      poster:
+        "https://www.wallpapertip.com/wmimgs/1-10435_attack-on-titan-season-3-wallpaper-hd.png"
+    },
+    {
+      name: "My Hero Academia",
+      releaseDate: "2016",
+      ratingIMDb: "8.5/10",
+      director: "Kohei Horikoshi",
+      poster:
+        "https://files.wallpaperpass.com/2019/10/bnha%20wallpaper%20186%20-%201920x1080.jpg"
+    },
+    {
+      name: "One Punch Man",
+      releaseDate: "2015",
+      ratingIMDb: "8.8/10",
+      director: "Yusuke Murata",
+      poster:
+        "https://www.wallpapertip.com/wmimgs/158-1581414_one-punch-man-s-class-heroes-characters-4k.jpg"
+    },
+    {
+      name: "Avatar: The Last Airbender",
+      releaseDate: "2005",
+      ratingIMDb: "9.2/10",
+      director: "Michael Dante DiMartino and Bryan Konietzko",
+      poster:
+        "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/5298bac0-b8bf-4c80-af67-725c1272dbb0/de1jch0-0627d112-5c34-44ec-bc37-d69296047420.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvNTI5OGJhYzAtYjhiZi00YzgwLWFmNjctNzI1YzEyNzJkYmIwXC9kZTFqY2gwLTA2MjdkMTEyLTVjMzQtNDRlYy1iYzM3LWQ2OTI5NjA0NzQyMC5qcGcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.pEF4PgsM9TPST-BEybkOaRgDwg7h8CN_ejnt5zyNPTU"
+    },
+    {
+      name: "Inazuma Eleven",
+      releaseDate: "2008",
+      ratingIMDb: "8/10",
+      director: "Tenya Yabuno",
+      poster: "https://wallpaperaccess.com/full/1623339.jpg"
     }
   ]
 };
