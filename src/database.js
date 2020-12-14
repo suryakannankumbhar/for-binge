fc1f01268481d682710aa36ab8557a73628fb66f
Surya Kannan Kumbhar <suryakannankumbhar@gmail.com>
2020-12-14 08:13:15 +0530

    update - updated database

--- src/database.js
+++ src/database.js
@@ -1,18 +1,19 @@
 export const database = {
   all: [
     {
-      name: "The Incredible Hulk",
-      releaseDate: "2008",
-      ratingIMDb: "6.7/10",
-      director: "Louis Leterrier",
-      poster: "https://wallpapercave.com/wp/2Cf6ajq.jpg"
-    },
-    {
       name: "Iron Man",
       releaseDate: "2008",
       ratingIMDb: "7.9/10",
       director: "Jon Faverou",
       poster: "https://wallpaperaccess.com/full/1653412.jpg"
+    },
+    {
+      name: "The Incredible Hulk",
+      releaseDate: "2008",
+      ratingIMDb: "6.7/10",
+      director: "Louis Leterrier",
+      poster:
+        "https://www.joblo.com/assets/images/oldsite/posters/images/full/2008-incredible_hulk-2.jpg"
     },
     {
       name: "Iron Man 2",
@@ -22,13 +23,6 @@ export const database = {
       poster: "https://images8.alphacoders.com/662/662512.jpg"
     },
     {
-      name: "Iron Man 3",
-      releaseDate: "2013",
-      ratingIMDb: "7.2/10",
-      director: "Shane Black",
-      poster: "https://images8.alphacoders.com/378/thumb-1920-378546.jpg"
-    },
-    {
       name: "Thor",
       releaseDate: "2011",
       ratingIMDb: "7/10",
@@ -36,18 +30,12 @@ export const database = {
       poster: "https://wallpaperaccess.com/full/645142.jpg"
     },
     {
-      name: "Thor The Dark World",
-      releaseDate: "2013",
+      name: "Captain America: The First Avenger",
+      releaseDate: "2011",
       ratingIMDb: "6.9/10",
-      director: "Alan Taylor",
-      poster: "https://wallpapercave.com/wp/wp3079593.jpg"
-    },
-    {
-      name: "Thor: Ragnarok",
-      releaseDate: "2017",
-      ratingIMDb: "7.9/10",
-      director: "Taika Waititi",
-      poster: "https://7wallpapers.net/wp-content/uploads/7_Thor-Ragnarok.jpg"
+      director: "Joe Johnston",
+      poster:
+        "https://www.clandestinecritic.co.uk/wp-content/uploads/2011/08/Captain-America-First-Avenger-poster-1100x826.jpg"
     },
     {
       name: "The Avengers",
@@ -57,6 +45,37 @@ export const database = {
       poster: "https://wallpapercave.com/wp/6lvp4zT.jpg"
     },
     {
+      name: "Iron Man 3",
+      releaseDate: "2013",
+      ratingIMDb: "7.2/10",
+      director: "Shane Black",
+      poster: "https://images8.alphacoders.com/378/thumb-1920-378546.jpg"
+    },
+
+    {
+      name: "Thor The Dark World",
+      releaseDate: "2013",
+      ratingIMDb: "6.9/10",
+      director: "Alan Taylor",
+      poster: "https://wallpapercave.com/wp/wp3079593.jpg"
+    },
+    {
+      name: "Captain America: The Winter Soldier",
+      releaseDate: "2014",
+      ratingIMDb: "7.7/10",
+      director: "Anthony and Joe Russo",
+      poster:
+        "https://williamsilvia.files.wordpress.com/2015/01/captain-america-the-winter-soldier-movie-poster-2.jpg"
+    },
+    {
+      name: "Guardians Of The Galaxy Vol. 1",
+      releaseDate: "2014",
+      ratingIMDb: "8/10",
+      director: "James Gunn",
+      poster:
+        "https://d2eib6r9tuf5y8.cloudfront.net/l/assets/img/article/article-1342-kbswtvq2/keyvisual.jpg"
+    },
+    {
       name: "Avengers:Age Of Ultron",
       releaseDate: "2015",
       ratingIMDb: "7.3/10",
@@ -64,13 +83,104 @@ export const database = {
       poster: "https://wallpapercave.com/wp/wp1818455.jpg"
     },
     {
+      name: "Ant-Man",
+      releaseDate: "2015",
+      ratingIMDb: "7.3/10",
+      director: "Peyton Reed",
+      poster:
+        "https://www.femalefirst.co.uk/image-library/land/1000/a/ant-man-mondo-poster.jpg"
+    },
+    {
+      name: "Captain America: Civil War",
+      releaseDate: "2016",
+      ratingIMDb: "7.8/10",
+      director: "Anthony and Joe Russo",
+      poster:
+        "https://images.saymedia-content.com/.image/t_share/MTc0NDE3NDM5OTY5MjU3MDk0/should-i-watch-captain-america-civil-war.jpg"
+    },
+    {
+      name: "Doctor Strange",
+      releaseDate: "2016",
+      ratingIMDb: "7.5/10",
+      director: "Scott Derrickson",
+      poster:
+        "https://hcmoviereviews.files.wordpress.com/2016/10/doctor-strange-quad-poster.jpg"
+    },
+    {
+      name: "Guardians Of The Galaxy Vol. 2",
+      releaseDate: "2017",
+      ratingIMDb: "7.6/10",
+      director: "James Gunn",
+      poster:
+        "https://cdn.shopify.com/s/files/1/1351/2613/products/IMG_8632.jpg?v=1525777822"
+    },
+    {
+      name: "Spider-Man: Homecoming",
+      releaseDate: "2017",
+      ratingIMDb: "7.4/10",
+      director: "Jon Watts",
+      poster:
+        "https://i2.wp.com/filmyfool.com/wp-content/uploads/2020/07/Spider-Home.jpg?resize=1170%2C658"
+    },
+    {
+      name: "Thor: Ragnarok",
+      releaseDate: "2017",
+      ratingIMDb: "7.9/10",
+      director: "Taika Waititi",
+      poster: "https://7wallpapers.net/wp-content/uploads/7_Thor-Ragnarok.jpg"
+    },
+    {
+      name: "Black Panther",
+      releaseDate: "2018",
+      ratingIMDb: "7.3/10",
+      director: "Ryan Coogler",
+      poster:
+        "https://www.newstatesman.com/sites/default/files/styles/cropped_article_image/public/blogs_2018/02/black-panther-quad-poster.jpg?itok=5R6rS8m4"
+    },
+    {
       name: "Avengers: Infinity War",
       releaseDate: "2018",
       ratingIMDb: "8.4/10",
       director: "Joe Russo, Anthony Russo",
       poster:
-        "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/5298bac0-b8bf-4c80-af67-725c1272dbb0/dbqyhh7-1aaad27e-1e5c-4fa1-96c5-7a888a6dbefe.jpg/v1/fill/w_1192,h_670,q_70,strp/avengers__infinity_war_wallpaper_by_thekingblader995_dbqyhh7-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3siaGVpZ2h0IjoiPD0xMDgwIiwicGF0aCI6IlwvZlwvNTI5OGJhYzAtYjhiZi00YzgwLWFmNjctNzI1YzEyNzJkYmIwXC9kYnF5aGg3LTFhYWFkMjdlLTFlNWMtNGZhMS05NmM1LTdhODg4YTZkYmVmZS5qcGciLCJ3aWR0aCI6Ijw9MTkyMCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.xYiCwtw4AoZnU7TxQHDX7Lae60bM59yM-1l6JjmaPF0"
+        "https://hips.hearstapps.com/digitalspyuk.cdnds.net/18/14/1522924460-avengers-infinity-war-poster.jpg?crop=1xw:0.8888888888888888xh;center,top&resize=1200:*"
     },
+    {
+      name: "Ant-Man And The Wasp",
+      releaseDate: "2018",
+      ratingIMDb: "7.1/10",
+      director: "Peyton Reed",
+      poster:
+        "https://i.pinimg.com/originals/7b/6d/a4/7b6da4c04ef69ef99c9276226da6eea1.jpg"
+    },
+    {
+      name: "Captain Marvel",
+      releaseDate: "2019",
+      ratingIMDb: "6.9/10",
+      director: "Anna Boden, Ryan Fleck",
+      poster:
+        "https://images6.fanpop.com/image/photos/42600000/Captain-Marvel-2019-marvels-captain-marvel-42625134-1600-900.png"
+    },
+    {
+      name: "Avengers: End Game",
+      releaseDate: "2019",
+      ratingIMDb: "8.4/10",
+      director: "Joe Russo, Anthony Russo",
+      poster:
+        "https://www.glitched.online/wp-content/uploads/2019/05/Avengers-Endgame.jpeg"
+    },
+    {
+      name: "Spider-Man: Far From Home",
+      releaseDate: "2019",
+      ratingIMDb: "7.5/10",
+      director: "Jon Watts",
+      poster:
+        "https://cdn1-www.superherohype.com/assets/uploads/gallery/spider-man-far-from-home/spider-man-far-from-home-poster.jpg"
+    },
+
+    // DC---------------------------------------------------------------------------
+    // ----------------------------------------------------------------------------------
+    // ------------------------------------------------------------------------------
     {
       name: "Batman Begins",
       releaseDate: "2005",
@@ -125,18 +235,19 @@ export const database = {
   ],
   marvel: [
     {
-      name: "The Incredible Hulk",
-      releaseDate: "2008",
-      ratingIMDb: "6.7/10",
-      director: "Louis Leterrier",
-      poster: "https://wallpapercave.com/wp/2Cf6ajq.jpg"
-    },
-    {
       name: "Iron Man",
       releaseDate: "2008",
       ratingIMDb: "7.9/10",
       director: "Jon Faverou",
       poster: "https://wallpaperaccess.com/full/1653412.jpg"
+    },
+    {
+      name: "The Incredible Hulk",
+      releaseDate: "2008",
+      ratingIMDb: "6.7/10",
+      director: "Louis Leterrier",
+      poster:
+        "https://www.joblo.com/assets/images/oldsite/posters/images/full/2008-incredible_hulk-2.jpg"
     },
     {
       name: "Iron Man 2",
@@ -146,13 +257,6 @@ export const database = {
       poster: "https://images8.alphacoders.com/662/662512.jpg"
     },
     {
-      name: "Iron Man 3",
-      releaseDate: "2013",
-      ratingIMDb: "7.2/10",
-      director: "Shane Black",
-      poster: "https://images8.alphacoders.com/378/thumb-1920-378546.jpg"
-    },
-    {
       name: "Thor",
       releaseDate: "2011",
       ratingIMDb: "7/10",
@@ -160,18 +264,12 @@ export const database = {
       poster: "https://wallpaperaccess.com/full/645142.jpg"
     },
     {
-      name: "Thor The Dark World",
-      releaseDate: "2013",
+      name: "Captain America: The First Avenger",
+      releaseDate: "2011",
       ratingIMDb: "6.9/10",
-      director: "Alan Taylor",
-      poster: "https://wallpapercave.com/wp/wp3079593.jpg"
-    },
-    {
-      name: "Thor: Ragnarok",
-      releaseDate: "2017",
-      ratingIMDb: "7.9/10",
-      director: "Taika Waititi",
-      poster: "https://7wallpapers.net/wp-content/uploads/7_Thor-Ragnarok.jpg"
+      director: "Joe Johnston",
+      poster:
+        "https://www.clandestinecritic.co.uk/wp-content/uploads/2011/08/Captain-America-First-Avenger-poster-1100x826.jpg"
     },
     {
       name: "The Avengers",
@@ -181,6 +279,37 @@ export const database = {
       poster: "https://wallpapercave.com/wp/6lvp4zT.jpg"
     },
     {
+      name: "Iron Man 3",
+      releaseDate: "2013",
+      ratingIMDb: "7.2/10",
+      director: "Shane Black",
+      poster: "https://images8.alphacoders.com/378/thumb-1920-378546.jpg"
+    },
+
+    {
+      name: "Thor The Dark World",
+      releaseDate: "2013",
+      ratingIMDb: "6.9/10",
+      director: "Alan Taylor",
+      poster: "https://wallpapercave.com/wp/wp3079593.jpg"
+    },
+    {
+      name: "Captain America: The Winter Soldier",
+      releaseDate: "2014",
+      ratingIMDb: "7.7/10",
+      director: "Anthony and Joe Russo",
+      poster:
+        "https://williamsilvia.files.wordpress.com/2015/01/captain-america-the-winter-soldier-movie-poster-2.jpg"
+    },
+    {
+      name: "Guardians Of The Galaxy Vol. 1",
+      releaseDate: "2014",
+      ratingIMDb: "8/10",
+      director: "James Gunn",
+      poster:
+        "https://d2eib6r9tuf5y8.cloudfront.net/l/assets/img/article/article-1342-kbswtvq2/keyvisual.jpg"
+    },
+    {
       name: "Avengers:Age Of Ultron",
       releaseDate: "2015",
       ratingIMDb: "7.3/10",
@@ -188,12 +317,99 @@ export const database = {
       poster: "https://wallpapercave.com/wp/wp1818455.jpg"
     },
     {
+      name: "Ant-Man",
+      releaseDate: "2015",
+      ratingIMDb: "7.3/10",
+      director: "Peyton Reed",
+      poster:
+        "https://www.femalefirst.co.uk/image-library/land/1000/a/ant-man-mondo-poster.jpg"
+    },
+    {
+      name: "Captain America: Civil War",
+      releaseDate: "2016",
+      ratingIMDb: "7.8/10",
+      director: "Anthony and Joe Russo",
+      poster:
+        "https://images.saymedia-content.com/.image/t_share/MTc0NDE3NDM5OTY5MjU3MDk0/should-i-watch-captain-america-civil-war.jpg"
+    },
+    {
+      name: "Doctor Strange",
+      releaseDate: "2016",
+      ratingIMDb: "7.5/10",
+      director: "Scott Derrickson",
+      poster:
+        "https://hcmoviereviews.files.wordpress.com/2016/10/doctor-strange-quad-poster.jpg"
+    },
+    {
+      name: "Guardians Of The Galaxy Vol. 2",
+      releaseDate: "2017",
+      ratingIMDb: "7.6/10",
+      director: "James Gunn",
+      poster:
+        "https://cdn.shopify.com/s/files/1/1351/2613/products/IMG_8632.jpg?v=1525777822"
+    },
+    {
+      name: "Spider-Man: Homecoming",
+      releaseDate: "2017",
+      ratingIMDb: "7.4/10",
+      director: "Jon Watts",
+      poster:
+        "https://i2.wp.com/filmyfool.com/wp-content/uploads/2020/07/Spider-Home.jpg?resize=1170%2C658"
+    },
+    {
+      name: "Thor: Ragnarok",
+      releaseDate: "2017",
+      ratingIMDb: "7.9/10",
+      director: "Taika Waititi",
+      poster: "https://7wallpapers.net/wp-content/uploads/7_Thor-Ragnarok.jpg"
+    },
+    {
+      name: "Black Panther",
+      releaseDate: "2018",
+      ratingIMDb: "7.3/10",
+      director: "Ryan Coogler",
+      poster:
+        "https://www.newstatesman.com/sites/default/files/styles/cropped_article_image/public/blogs_2018/02/black-panther-quad-poster.jpg?itok=5R6rS8m4"
+    },
+    {
       name: "Avengers: Infinity War",
       releaseDate: "2018",
       ratingIMDb: "8.4/10",
       director: "Joe Russo, Anthony Russo",
       poster:
-        "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/5298bac0-b8bf-4c80-af67-725c1272dbb0/dbqyhh7-1aaad27e-1e5c-4fa1-96c5-7a888a6dbefe.jpg/v1/fill/w_1192,h_670,q_70,strp/avengers__infinity_war_wallpaper_by_thekingblader995_dbqyhh7-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3siaGVpZ2h0IjoiPD0xMDgwIiwicGF0aCI6IlwvZlwvNTI5OGJhYzAtYjhiZi00YzgwLWFmNjctNzI1YzEyNzJkYmIwXC9kYnF5aGg3LTFhYWFkMjdlLTFlNWMtNGZhMS05NmM1LTdhODg4YTZkYmVmZS5qcGciLCJ3aWR0aCI6Ijw9MTkyMCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.xYiCwtw4AoZnU7TxQHDX7Lae60bM59yM-1l6JjmaPF0"
+        "https://hips.hearstapps.com/digitalspyuk.cdnds.net/18/14/1522924460-avengers-infinity-war-poster.jpg?crop=1xw:0.8888888888888888xh;center,top&resize=1200:*"
+    },
+    {
+      name: "Ant-Man And The Wasp",
+      releaseDate: "2018",
+      ratingIMDb: "7.1/10",
+      director: "Peyton Reed",
+      poster:
+        "https://i.pinimg.com/originals/7b/6d/a4/7b6da4c04ef69ef99c9276226da6eea1.jpg"
+    },
+    {
+      name: "Captain Marvel",
+      releaseDate: "2019",
+      ratingIMDb: "6.9/10",
+      director: "Anna Boden, Ryan Fleck",
+      poster:
+        "https://images6.fanpop.com/image/photos/42600000/Captain-Marvel-2019-marvels-captain-marvel-42625134-1600-900.png"
+    },
+    {
+      name: "Avengers: End Game",
+      releaseDate: "2019",
+      ratingIMDb: "8.4/10",
+      director: "Joe Russo, Anthony Russo",
+      poster:
+        "https://www.glitched.online/wp-content/uploads/2019/05/Avengers-Endgame.jpeg"
+    },
+    {
+      name: "Spider-Man: Far From Home",
+      releaseDate: "2019",
+      ratingIMDb: "7.5/10",
+      director: "Jon Watts",
+      poster:
+        "https://cdn1-www.superherohype.com/assets/uploads/gallery/spider-man-far-from-home/spider-man-far-from-home-poster.jpg"
     }
   ],
   dc: [
